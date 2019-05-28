383019bdafaafc092b5a56c9630b45bd702125cd
quizforparty44 <quizforparty44@gmail.com>
2019-05-28 09:11:31 +0200

    first commit

---
+++ src/main/java/com/example/demo/JxdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxdemoApplication.class, args);
+	}
+
+}
